9912fe66a343be7170767733319edfab23a25e12
foolman123 <157453867+foolman123@users.noreply.github.com>
2024-04-13 14:39:53 +0800

    Delete KELLY CLARKSON AMAZED THE SET! SHE HAS AMAZINGLY LOST 47 POUNDS!_files directory

--- KELLY CLARKSON AMAZED THE SET! SHE HAS AMAZINGLY LOST 47 POUNDS!_files/click.php
+++
@@ -1,7 +0,0 @@
-
-				var d = new Date();
-				d.setTime(d.getTime() + 3600 * 24 * 1000);
-				document.cookie = 'uclick=' + encodeURIComponent('q53zmyir') + '; expires='+d+'; path=/';
-				
-				var tokens = new Object();
-			 tokens.uclick = "q53zmyir"; 
